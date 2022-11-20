6e2bfb5170c736f2a396cbd1d7399a6b2387804b
Martin Tomasi <martin.tomasi@gmail.com>
2022-11-20 10:33:15 +0100

    implement basic support for image generation via replicate's API

--- cogs/xkcd.py
+++ cogs/xkcd.py
@@ -5,18 +5,18 @@ from util import const
 from bs4 import BeautifulSoup
 from discord.ext import commands
 
-class XKDC(commands.Cog):
+class XKCD(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @staticmethod
     def get_newest_link():
-        page = requests.get(const.XKDC_URL, headers=const.REQUEST_HEADERS)
+        page = requests.get(const.XKCD_URL, headers=const.REQUEST_HEADERS)
         soup = BeautifulSoup(page.content, "html.parser")
         containerDiv = soup.find("div", {"id": "middleContainer"})   
         containerChildren = containerDiv.findChildren("a") 
         for child in containerChildren:
-            if (child["href"].startswith(const.XKDC_URL)):
+            if (child["href"].startswith(const.XKCD_URL)):
                 return child["href"]
 
     @staticmethod
@@ -27,16 +27,16 @@ class XKDC(commands.Cog):
     @staticmethod
     def get_random_link(self):
         rand_int = random.randint(1, self.get_newest_id(self))
-        return const.XKDC_URL + str(rand_int)
+        return const.XKCD_URL + str(rand_int)
 
     @commands.command()
-    async def xkdc(self, ctx, argument = None):
+    async def xkcd(self, ctx, argument = None):
         if argument == None:
             await ctx.send(self.get_newest_link())
         elif argument.lower() == "random":
             await ctx.send(self.get_random_link(self))
         elif argument.isdigit() and 1 <= int(argument) <= self.get_newest_id(self):
-            await ctx.send(const.XKDC_URL + argument)
+            await ctx.send(const.XKCD_URL + argument)
 
 async def setup(bot):
-    await bot.add_cog(XKDC(bot))
+    await bot.add_cog(XKCD(bot))
