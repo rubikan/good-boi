397d69868fcc66bac06c74dceb41008bed60fcd9
Martin Tomasi <martin.tomasi@gmail.com>
2024-03-20 16:55:19 +0100

    only edit the message after 5 characters

--- cogs/ai.py
+++ cogs/ai.py
@@ -9,28 +9,34 @@ class AI(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.client = OpenAI(
-            base_url=bot.config.openai.baseUrl,
-            api_key=bot.config.openai.apiKey,
+            base_url=bot.config.openai.base_url,
+            api_key=bot.config.openai.api_key,
         )
 
     @commands.command()
     async def chat(self, ctx: Context, *, arg: str):
-        msg = await ctx.send("Generating answer...")
+        msg = await ctx.reply("I'm thinking...")
         async with ctx.typing():
             result = self.client.chat.completions.create(
                 model=self.bot.config.openai.model,
                 messages=[
-                    {"role": "system", "content": self.bot.config.openai.chatPrompt},
+                    {"role": "system", "content": self.bot.config.openai.chat_prompt},
                     {"role": "user", "content": arg},
                 ],
                 stream=True,
             )
             answer = ""
+            delta_len = 0
             for chunk in result:
                 content = chunk.choices[0].delta.content
                 if content:
+                    print("delta countent:", content)
                     answer += content
-                    await msg.edit(content=answer)
+                    delta_len += len(content)
+                    if delta_len > 5:
+                        print("delta_len:", delta_len)
+                        await msg.edit(content=answer)
+                        delta_len = 0
 
 
 async def setup(bot):
